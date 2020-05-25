17a1244eff121e2ad562212bde7bfaa70183ceaa
gozenner <daniel.green@gozenner.com>
2020-05-25 15:07:40 +0300

    Fix migrations directory Add missing fields for event

---
+++ src/entity/CommsEvent.ts
@@ -0,0 +1,30 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn
+} from "typeorm";
+
+@Entity()
+export default class CommsEvent {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @CreateDateColumn()
+    created: Date;
+
+    @Column()
+    from: string;
+
+    @Column()
+    to: string;
+
+    @Column()
+    source: string;
+
+    @Column({nullable: true})
+    human: boolean;
+
+    @Column()
+    message: string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,20 @@
 import "reflect-metadata";
 
 import {createConnection} from "typeorm";
-import Event from "./entity/Event";
+import CommsEvent from "./entity/CommsEvent";
 
 (async () => {
     const conn = await createConnection();
 
     console.log('PG connected');
 
-    const event = new Event();
-    event.name = "Test";
+    const event = new CommsEvent();
+
+    event.from = "972523203925";
+    event.to = "32312312345";
+    event.source = "whatsapp";
+    event.message = "Hello!"
+
     await conn.manager.save(event);
 
     await conn.close();
